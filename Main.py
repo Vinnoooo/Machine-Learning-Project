9947754d94b8bef3b98c80bd7e197be636a26a8f
eddie <eddiesuberts04@gmail.com>
2025-10-07 20:49:22 +1100

    New Code

--- Main.py
+++ Main.py
@@ -2,13 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # 1. Create a signal: sum of two sine waves
-t = np.linspace(0, 1, 1000)  # 1 second, 1000 samples
+t = np.linspace(0, 5, 1000)  # 5 second, 1000 samples
 f1, f2 = 5, 20               # frequencies in Hz
 signal = np.sin(2*np.pi*f1*t) + 0.5*np.sin(2*np.pi*f2*t)
 
 # 2. Compute FFT
 F = np.fft.fft(signal)
-freq = np.fft.fftfreq(len(signal), d=(t[1]-t[0]))
+freq = np.fft.fftfreq(len(signal), d=(t[10]-t[0]))
 
 # --- First figure: FFT Magnitude Spectrum ---
 mask = freq >= 0
@@ -21,7 +21,8 @@ plt.grid(True)
 
 # --- Second figure: Original vs Filtered Signal ---
 fft_coeffs = np.fft.fft(signal)
-fft_coeffs[10:-10] = 0           # zero out high-frequency components
+range = 50
+fft_coeffs[range:-range] = 0           # zero out high-frequency components
 filtered_signal = np.fft.ifft(fft_coeffs)
 
 plt.figure(figsize=(10,4))        # Create a new figure for the second plot
